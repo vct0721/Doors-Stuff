76eb872945de4ddcdd39f9596c30fcef8bb455d1
vct0721 <166570696+vct0721@users.noreply.github.com>
2024-09-14 13:32:56 -0300

    Update TryHard Mode Loader.lua

--- TryHard Mode Loader.lua
+++ TryHard Mode Loader.lua
@@ -1,26 +1,16 @@
 if game.ReplicatedStorage.GameData.LatestRoom.Value > 0 or game.Players.LocalPlayer.Character.Humanoid.Health < 1 then
-    require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("You need to load the mode before door 1 and you need to be alive!", true)
-	wait(1)
-	game.Players.LocalPlayer.Character.Humanoid.Health = -100
+    game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game.caption("You need to load the mode before door 1 and you need to be alive!", true)
+    wait(1)
+    game.Players.LocalPlayer.Character.Humanoid.Health = -999
     game.ReplicatedStorage.GameStats["Player_".. game.Players.LocalPlayer.Name].Total.DeathCause.Value = "Did not load before door 1"
-	
 end
 
 if game:GetService("ReplicatedStorage").GameData.Floor.Value == "Retro" then
-
-game.ReplicatedStorage.GameData.LatestRoom.Changed:Connect(function()
-
-game.Players.LocalPlayer:Kick("Shoutout for hacking HC Remake")	
-
-elseif game:GetService("ReplicatedStorage").GameData.Floor.Value == "Hotel" then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/vct0721/Doors-Stuff/main/Try%20Hard%20Mode"))()
-
-elseif game:GetService("ReplicatedStorage").GameData.Floor.Value == "Mines" then 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/vct0721/Doors-Stuff/main/TryHard%20Floor%202"))()
-
-elseif game.ReplicatedStorage.GameData.Floor.Value == "Rooms" then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/vct0721/Doors-Stuff/main/Try%20Hard%20Mode"))()
-
-elseif game.ReplicatedStorage.GameData.Floor.Value == "Backdoor" then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/vct0721/Doors-Stuff/main/Try%20Hard%20Mode"))()
-
+    game.ReplicatedStorage.GameData.LatestRoom.Changed:Connect(function()
+        game.Players.LocalPlayer:Kick("No More Retro Mode")
+    end)
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/vct0721/Doors-Stuff/main/Try%20Hard%20Mode"))()
+    wait(1)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/vct0721/Doors-Stuff/main/Hard%20Modifiers"))()
+end
