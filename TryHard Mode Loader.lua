df294d19562d5593ab66d1bbb9b171b7dae4f543
vct0721 <166570696+vct0721@users.noreply.github.com>
2024-10-19 20:07:18 -0300

    Update TryHard Mode Loader.lua

--- TryHard Mode Loader.lua
+++ TryHard Mode Loader.lua
@@ -1,13 +1,17 @@
 local SyncHelper = loadstring(game:HttpGet("https://raw.githubusercontent.com/ChronoAcceleration/Comet-Development/refs/heads/main/Doors/Utility/SyncHelper.lua"))()
+
 if game.ReplicatedStorage.GameData.LatestRoom.Value > 0 or game.Players.LocalPlayer.Character.Humanoid.Health < 1 then 
     require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("You need to load the mode before door 1 and you need to be alive!", true)
     SyncHelper:deltaWait(1)
-    game.Players.LocalPlayer.Character.Humanoid.Health = -100
+    game.Players.LocalPlayer.Character.Humanoid.Health = 0
     game.ReplicatedStorage.GameStats["Player_" .. game.Players.LocalPlayer.Name].Total.DeathCause.Value = "Did not load before door 1"
+
 elseif game:GetService("ReplicatedStorage").GameData.Floor.Value == "Retro" then 
     game.ReplicatedStorage.GameData.LatestRoom.Changed:Connect(function()
         game.Players.LocalPlayer:Kick("No More Retro Mode")
     end)
+
 else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/vct0721/Doors-Stuff/refs/heads/main/Try%20Hard%20Mode"))()
 end
+
