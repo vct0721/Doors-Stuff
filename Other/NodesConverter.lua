a74520ee8ff6b546d019506751611d19efc175ca
vct0721 <166570696+vct0721@users.noreply.github.com>
2024-12-06 01:24:25 -0300

    Update NodesConverter.lua

--- Other/NodesConverter.lua
+++ Other/NodesConverter.lua
@@ -31,15 +31,7 @@ local function convertToLegacy(Room: Model): ()
 
         local B = room:WaitForChild("RoomExit", .5):Clone()
 	B.Parent = room
-	B.Name = "RoomEnd"
-
-     	local C = room:WaitForChild("RoomEntrance", .5):Clone()
-       	A.Parent = room
-        A.Name = "Entrance"
-
-        local D = room:WaitForChild("RoomExit", .5):Clone()
-	B.Parent = room
-	B.Name = "Exit"		
+	B.Name = "RoomEnd"	
     end
 
     -- Tag the room as converted
