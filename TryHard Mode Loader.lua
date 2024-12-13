165e4ab81c4f197117a1ff90665f594a86c7ddb3
vct0721 <166570696+vct0721@users.noreply.github.com>
2024-12-13 00:34:31 -0300

    Update TryHard Mode Loader.lua

--- TryHard Mode Loader.lua
+++ TryHard Mode Loader.lua
@@ -1,12 +1,8 @@
 if game.ReplicatedStorage.GameData.LatestRoom.Value > 0 or game.Players.LocalPlayer.Character.Humanoid.Health <= 0 then 
     require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("You need to load the mode before door 1 and you need to be alive!", true)
-    wait(1)
+    wait(0.2)
     game.Players.LocalPlayer.Character.Humanoid.Health = -100
     game.ReplicatedStorage.GameStats["Player_" .. game.Players.LocalPlayer.Name].Total.DeathCause.Value = "Did not load before door 1"
-elseif game:GetService("ReplicatedStorage").GameData.Floor.Value == "Retro" then 
-    game.ReplicatedStorage.GameData.LatestRoom.Changed:Connect(function()
-        game.Players.LocalPlayer:Kick("No More TryHard Retro Mode")
-    end)
 else
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/vct0721/Doors-Stuff/refs/heads/main/Try%20Hard%20Mode"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/vct0721/Doors-Stuff/main/Try%20Hard%20Mode"))()
 end
