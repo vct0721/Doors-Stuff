f3c1200f2879375bd78ede55be6cf45f923dda1a
vct0721 <166570696+vct0721@users.noreply.github.com>
2025-01-02 23:49:34 -0300

    Update TryHard Mode Loader.lua

--- TryHard Mode Loader.lua
+++ TryHard Mode Loader.lua
@@ -4,5 +4,5 @@ if game.ReplicatedStorage.GameData.LatestRoom.Value > 0 or game.Players.LocalPla
     game.Players.LocalPlayer.Character.Humanoid.Health = -100
     game.ReplicatedStorage.GameStats["Player_" .. game.Players.LocalPlayer.Name].Total.DeathCause.Value = "Did not load before door 1"
 else
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/vct0721/Doors-Stuff/main/Try%20Hard%20Mode"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/vct0721/Doors-Stuff/refs/heads/main/Try%20Hard%20Mode.lua"))()
 end
